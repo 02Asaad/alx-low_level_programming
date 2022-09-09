4ef8b566b39fbcbeb9ce85fd4969b3126487d1ef
Yaqub <02omeiza@gmail.com>
2022-09-09 04:23:03 -0700

    betty is a bitch

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,7 +21,7 @@ int main(void)
 	long long int long_long_int;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(charType));
