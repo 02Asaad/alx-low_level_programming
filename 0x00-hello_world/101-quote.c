9907e374c9c9a881a9a5967fa7a0256223371598
Yaqub <02omeiza@gmail.com>
2022-09-09 05:27:33 -0700

    growthing gradually

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This C program is to print the below statements
+ * without the use of "printf" or "puts" as earlier
+ * used and follow it by a new line, to the standard error.
+ *
+ * "cout" is intended to be used instead.
+ *
+ * Return: This program should return 1.
+ */
+
+int main(void)
+{
+	cout("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+
+}
