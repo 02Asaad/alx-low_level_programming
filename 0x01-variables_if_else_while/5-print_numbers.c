abfbea61b1a170268aef88cc6287cc9bbf8d1ae9
Yaqub <02omeiza@gmail.com>
2022-09-09 20:56:50 -0700

    fg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print single digit numbers in
+ * base 10, follow by a new line.
+ *
+ * Return: return to 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d\n", i);
+
+	return (0);
+}
