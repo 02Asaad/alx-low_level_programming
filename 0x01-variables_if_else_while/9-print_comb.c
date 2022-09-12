3be1f3073115396e5120a4fbf0992643b0f1811a
Yaqub <02omeiza@gmail.com>
2022-09-12 03:37:32 -0700

    gvb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This programm is to print all possible
+ * combinations of single-digit numbers.
+ *
+ * use only "putchar" but printf, puts etc.
+ *
+ * Return: always returnn a 0.
+ */
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	
+	putchar(i);
+	putchar(,);	
+	printf("\n");
+	return (0);
+}
