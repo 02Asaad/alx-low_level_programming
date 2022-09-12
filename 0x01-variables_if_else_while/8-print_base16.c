9ecd0617d537ae77b06013cd1e5059f34a4ed5de
Yaqub <02omeiza@gmail.com>
2022-09-12 02:38:26 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print single digit numbers in
+ * base 16, follow by a new line.
+ *
+ * Return: return to 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	putchar(i);
+	putchar("\n");
+	return (0);
+}
