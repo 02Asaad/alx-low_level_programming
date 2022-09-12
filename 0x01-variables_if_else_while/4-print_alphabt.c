15ab0ba2e7f378d7f73f7c5f50807be58ec56120
Yaqub <02omeiza@gmail.com>
2022-09-11 17:45:50 -0700

    eq

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,13 +14,13 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; ++c) {
+	for (c = 'a'; c <= 'z'; ++c)
 	if (c == 'e')
 	continue;
 
 	if (c == 'q')
 	continue;
-	}
+
 	putchar(c);
 	putchar('\n');
 	return (0);
