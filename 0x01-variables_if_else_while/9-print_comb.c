801956bcc639d4473dba6a6f6c73cbe6799a34dc
Yaqub <02omeiza@gmail.com>
2022-09-12 12:28:15 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,13 @@ int main(void)
 {
 	int y;
 
-	for (y = 48; y < 57; y++)
+	for (y = 48; y <= 57; y++)
 	putchar(y);
 	if (y != 57)
-
+	{
 	putchar(',');
 	putchar(' ');
-
+	}
 	putchar('\n');
 	return (0);
 
