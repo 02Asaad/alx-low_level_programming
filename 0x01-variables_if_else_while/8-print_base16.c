98c10ef0ae1720c90f5218f488f178db658dc06a
Yaqub <02omeiza@gmail.com>
2022-09-12 09:27:01 -0700

    rig

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,14 @@
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; i < 16; ++i)
-
+	for (i = 0; i <= 9; ++i)
 	putchar(i);
+
+	for (i = 'a'; i <= 'e'; ++i)
+	putchar(i);
+
 	putchar('\n');
 
 	return (0);
