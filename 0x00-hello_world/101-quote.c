9d9cd3a36fc0dc2e913590ae3a7b84c8f91e4976
Yaqub <02omeiza@gmail.com>
2022-09-12 21:57:02 -0700

    tt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
 /**
+ * main - Programm about a quote.
  * putchar - This C program is to print the below statements
  * without the use of "printf" or "puts" as earlier
  * used and follow it by a new line, to the standard error.
@@ -10,12 +11,11 @@
  * Return: This program should return 1.
  */
 
-int main(void) 
-
+int main(void)
 {
 	write(2,
 
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n		",59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 
 
