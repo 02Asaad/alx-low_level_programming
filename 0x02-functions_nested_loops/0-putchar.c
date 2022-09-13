f93ba54767414440fbc558fe6dca111efaa57306
Yaqub <02omeiza@gmail.com>
2022-09-13 13:37:01 -0700

    Anavami

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program is about writing a
+ * program that prints "_putchar" follow by a new line.
+ *
+ * The program should return 0.
+ *
+ * Return: return 0.
+ */
+
+int main(void)
+{
+	int i = _putchar;
+
+	putchar(i);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This program is about Functions and Nesting Loops
