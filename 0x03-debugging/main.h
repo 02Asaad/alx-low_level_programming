e40707e065247063f0121a53f7b857f37aebd97e
Yaqub <02omeiza@gmail.com>
2022-09-15 16:58:16 -0700

    roadmap

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void positive_or_negative(int i);
+int main(void)
+{
+int i;
 
+i = 0;
+positive_or_negative(i);
+
+return (0);
