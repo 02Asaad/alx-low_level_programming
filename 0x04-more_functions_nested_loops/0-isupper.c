57c6673b7e88d9e8383d70d180dc1e17ce1f11c0
Yaqub <02omeiza@gmail.com>
2022-09-15 19:50:23 -0700

    ring

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - check for codes
+ *
+ * Return: As alwayss, 0.
+ */
+int _isupper(int c);
+{
+	int c = A, a;
+
+	if (c == 'A')
+	{
+	printf("%c", _isupper(int c));
+	return (1);
+	}
+	else 
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This program is a continuation of more nested loops in C programming
