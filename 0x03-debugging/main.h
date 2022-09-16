ae5938b6013a8413d5782fc9c3e66f59310ce5b1
Yaqub <02omeiza@gmail.com>
2022-09-15 18:36:07 -0700

    redbug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,10 +5,8 @@
 
 void positive_or_negative(int i);
 
-/**
- * main - short int largest_number(int, int, int);
- * void print_remaining_days(int, int, int);
- * int convert_day(int, int);
-*/
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
 #endif /* MAIN_H */
